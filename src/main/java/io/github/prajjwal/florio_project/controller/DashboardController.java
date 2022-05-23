7ecd8226748021641112e57a35b320b230635e0e
Prajjwal <prajjwal.pachauri_cs19@gla.ac.in>
2022-05-23 11:26:45 +0530

    Create a new files for dashboard

---
+++ src/main/java/io/github/prajjwal/florio_project/controller/DashboardController.java
@@ -0,0 +1,10 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 23-05-2022
+Time: 11:25 AM
+File: DashboarView.java */
+package io.github.prajjwal.florio_project.controller;
+
+public class DashboardController {
+}
