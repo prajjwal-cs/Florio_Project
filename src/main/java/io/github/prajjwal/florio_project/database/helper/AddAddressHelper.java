c372150acce828383feed6ff8c5d565b821473b9
prajjwal <prajjwal199275@gmail.com>
2022-05-25 22:52:00 +0530

    Create a new add address helper

--- src/main/java/io/github/prajjwal/florio_project/database/helper/AddAddressHelper.java
+++ src/main/java/io/github/prajjwal/florio_project/database/helper/AddAddressHelper.java
@@ -6,5 +6,19 @@ Time: 22:38
 File: AddAddressHelper.java */
 package io.github.prajjwal.florio_project.database.helper;
 
+import io.github.prajjwal.florio_project.database.DatabaseConnection;
+import io.github.prajjwal.florio_project.model.User;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AddAddressHelper {
+    public void addAddress(User user) throws SQLException {
+        DatabaseConnection connection = new DatabaseConnection();
+        String insertQuery =
+                "INSERT INTO user (Address) values ?)";
+        PreparedStatement statement = connection.getConnection().prepareStatement(insertQuery);
+        statement.setString(1, user.getAddress());
+        statement.executeUpdate();
+    }
 }
